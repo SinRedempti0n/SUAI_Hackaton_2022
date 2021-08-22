346ea0cbb1be6b8984e15f227f88d83cc45cb3c6
SinRedempti0n <68812690+SinRedempti0n@users.noreply.github.com>
2021-08-22 04:27:04 +0300

    Maps point add

--- app/src/main/java/com/example/hackaton/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/hackaton/ui/dashboard/DashboardFragment.kt
@@ -1,19 +1,24 @@
 package com.example.hackaton.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.model.LatLng
 import androidx.lifecycle.ViewModelProvider
+import com.example.hackaton.Place
 import com.example.hackaton.R
 import com.example.hackaton.databinding.FragmentDashboardBinding
+import com.example.hackaton.ui.home.CustomRecyclerAdapter
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class DashboardFragment : Fragment(), OnMapReadyCallback {
@@ -21,6 +26,9 @@ class DashboardFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
+
+    val USER_KEY = "Test1"
+    val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().getReference(USER_KEY)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,9 +51,25 @@ class DashboardFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(58.422, 32.3836)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Новгородская область"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val points: ArrayList<LatLng> = ArrayList()
+        mDatabase.get().addOnSuccessListener {
+            for(data in it.children){
+                Log.i("firebase", "Got value ${data.getValue().toString()}")
+                var id = "0"
+                val cord = LatLng(data.child("latitude").getValue().toString().toDouble(), data.child("longitude").getValue().toString().toDouble())
+                points.add(cord)
+                mMap.addMarker(MarkerOptions().position(cord).title(data.child("name").getValue().toString()))
+
+                var history = data.child("history").getValue().toString()
+                val sydney = LatLng(58.422, 32.3836)
+            }
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(points[0]))
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
+        }
+
     }
+
 }
+
 
